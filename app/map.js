3764d48ac51ba914950535be4cf919c867dd228b
simplebusiness26 <simplebussiness26@gmail.com>
2026-07-16 03:29:41 +0100

    Update map.js

--- app/map.js
+++ app/map.js
@@ -1,55 +1,100 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { supabase } from "../services/supabase";
+
 
 export default function MapScreen() {
+
+  const [places, setPlaces] = useState([]);
+
+
+  useEffect(() => {
+    loadPlaces();
+  }, []);
+
+
+  async function loadPlaces() {
+
+    const { data, error } = await supabase
+      .from("businesses")
+      .select("*");
+
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+
+    setPlaces(data);
+
+  }
+
+
   return (
+
     <View style={styles.container}>
 
       <Text style={styles.title}>
-        Guestbook Map
+        🗺️ Guestbook Map
       </Text>
 
-      <Text style={styles.text}>
-        🏡 Airbnbs
-      </Text>
 
-      <Text style={styles.text}>
-        ☕ Cafes
-      </Text>
+      <FlatList
 
-      <Text style={styles.text}>
-        🍺 Pubs
-      </Text>
+        data={places}
 
-      <Text style={styles.text}>
-        🍽 Restaurants
-      </Text>
+        keyExtractor={(item)=>item.id}
 
-      <Text style={styles.text}>
-        🌳 Parks
-      </Text>
+        renderItem={({item})=>(
+
+          <View style={styles.card}>
+
+            <Text style={styles.name}>
+              {item.name}
+            </Text>
+
+            <Text>
+              {item.category}
+            </Text>
+
+          </View>
+
+        )}
+
+      />
+
 
     </View>
+
   );
+
 }
+
 
 const styles = StyleSheet.create({
 
-  container:{
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center"
-  },
+container:{
+flex:1,
+padding:20
+},
 
-  title:{
-    fontSize:28,
-    fontWeight:"bold",
-    marginBottom:30
-  },
+title:{
+fontSize:28,
+fontWeight:"bold",
+marginBottom:20
+},
 
-  text:{
-    fontSize:20,
-    margin:8
-  }
+card:{
+padding:15,
+borderWidth:1,
+borderRadius:10,
+marginBottom:10
+},
+
+name:{
+fontSize:20,
+fontWeight:"bold"
+}
 
 });
