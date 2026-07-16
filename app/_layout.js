0a3984bbe59313958379683de72448052cddfaf6
simplebusiness26 <simplebussiness26@gmail.com>
2026-07-16 02:54:53 +0100

    Update _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,55 @@
 import React from "react";
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 
 export default function Layout() {
+
   return (
-    <Stack
+
+    <Tabs
       screenOptions={{
-        headerShown: false
+        headerShown:false,
+        tabBarActiveTintColor:"#000000"
       }}
-    />
+
+    >
+
+      <Tabs.Screen
+        name="index"
+        options={{
+          title:"Home"
+        }}
+      />
+
+      <Tabs.Screen
+        name="map"
+        options={{
+          title:"Map"
+        }}
+      />
+
+      <Tabs.Screen
+        name="scan"
+        options={{
+          title:"Scan"
+        }}
+      />
+
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title:"Saved"
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title:"Profile"
+        }}
+      />
+
+    </Tabs>
+
   );
+
 }
