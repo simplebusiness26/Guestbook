d73d55868e65df70ef724a208478211eca657fa3
simplebusiness26 <simplebussiness26@gmail.com>
2026-07-20 02:16:30 +0100

    Update QRCodeGenerator.js

--- components/QRCodeGenerator.js
+++ components/QRCodeGenerator.js
@@ -5,8 +5,8 @@ import QRCode from "react-native-qrcode-svg";
 export default function QRCodeGenerator({propertyId}){
 
 
-const url =
-`https://guestbook.app/property/${propertyId}`;
+const url = 
+`https://guestbook.app/guest/${propertyId}`;
 
 
 return(
