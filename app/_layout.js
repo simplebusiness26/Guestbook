d5bea1385e122ab5955df0646860868248f8e5ff
simplebusiness26 <simplebussiness26@gmail.com>
2026-07-22 02:12:35 +0100

    Update _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { Stack } from "expo-router";
 
+import Header from "../components/Header";
+
 
 export default function Layout(){
 
@@ -11,38 +13,75 @@ return(
 
 screenOptions={{
 
-headerShown:false
+headerShown:true,
+
+header:()=> <Header />
 
 }}
 
 >
 
-<Stack.Screen name="index" />
+
+<Stack.Screen
+
+name="index"
+
+options={{
+
+headerShown:false
+
+}}
+
+/>
+
 
 <Stack.Screen name="map" />
 
+
 <Stack.Screen name="scan" />
+
 
 <Stack.Screen name="saved" />
 
+
 <Stack.Screen name="profile" />
 
+
+
+{/* Authentication */}
 
 <Stack.Screen name="auth/signup" />
 
 <Stack.Screen name="auth/login" />
 
+<Stack.Screen name="auth/verify" />
+
+
+
+{/* Business */}
 
 <Stack.Screen name="business/dashboard" />
 
+<Stack.Screen name="business/add" />
+
 <Stack.Screen name="business/reviews" />
 
+
+
+{/* Property */}
+
 <Stack.Screen name="property/dashboard" />
+
+<Stack.Screen name="property/add" />
 
 <Stack.Screen name="property/reviews" />
 
 
+
+{/* Admin */}
+
 <Stack.Screen name="admin/claims" />
+
 
 </Stack>
 
